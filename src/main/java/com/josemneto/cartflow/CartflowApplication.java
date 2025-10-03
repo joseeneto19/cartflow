168fcc6ebbc9a1e5ad543421d274a9313f2133e1
joseeneto <josemnetodev@gmail.com>
2025-10-03 10:57:58 -0300

    first commit

---
+++ src/main/java/com/josemneto/cartflow/CartflowApplication.java
@@ -0,0 +1,13 @@
+package com.josemneto.cartflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CartflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/josemneto/cartflow/CartflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.josemneto.cartflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartflowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
