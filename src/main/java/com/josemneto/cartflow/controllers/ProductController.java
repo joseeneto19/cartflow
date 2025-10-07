8d8a7eaba359df9b46585cd317efc2bf3865df10
joseeneto <josemnetodev@gmail.com>
2025-10-07 15:00:34 -0300

    feat: add findById endpoint to ProductController

--- src/main/java/com/josemneto/cartflow/controllers/ProductController.java
+++ src/main/java/com/josemneto/cartflow/controllers/ProductController.java
@@ -1,4 +1,24 @@
 package com.josemneto.cartflow.controllers;
 
+import com.josemneto.cartflow.dto.ProductDTO;
+import com.josemneto.cartflow.service.ProductService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/products")
 public class ProductController {
+
+    private ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ProductDTO findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
 }
