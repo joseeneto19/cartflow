16c06091b6b6e9e47147024155bafd613c20d62a
joseeneto <josemnetodev@gmail.com>
2025-10-07 15:00:04 -0300

    feat: add findById method to ProductService

--- src/main/java/com/josemneto/cartflow/service/ProductService.java
+++ src/main/java/com/josemneto/cartflow/service/ProductService.java
@@ -1,4 +1,27 @@
 package com.josemneto.cartflow.service;
 
+import com.josemneto.cartflow.dto.ProductDTO;
+import com.josemneto.cartflow.entities.Product;
+import com.josemneto.cartflow.repositories.ProductRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+    private ProductRepository repository;
+
+    public ProductService(ProductRepository repository) {
+        this.repository = repository;
+    }
+
+    @Transactional(readOnly = true)
+    public ProductDTO findById(Long id) {
+        Optional<Product> result = repository.findById(id);
+        Product product = result.get();
+        ProductDTO dto = new ProductDTO(product);
+        return dto;
+    }
 }
