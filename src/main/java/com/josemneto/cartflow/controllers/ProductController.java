9a93b005c78d1a6e937700c717dde58b068b2f79
joseeneto19 <josemnetodev@gmail.com>
2025-10-11 10:49:37 -0300

    feat: add method update and method copyDtoToEntity to ProductService and add endpoint update to ProductController

--- src/main/java/com/josemneto/cartflow/controllers/ProductController.java
+++ src/main/java/com/josemneto/cartflow/controllers/ProductController.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/products")
@@ -39,5 +38,11 @@ public class ProductController {
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}")
                 .buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<ProductDTO> update(@PathVariable Long id, @RequestBody ProductDTO dto) {
+        dto = service.update(id, dto);
+        return ResponseEntity.ok(dto);
     }
 }
