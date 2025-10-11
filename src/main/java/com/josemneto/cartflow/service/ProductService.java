5616823ab60d23e1737ba97b9cbece9956751aee
joseeneto19 <josemnetodev@gmail.com>
2025-10-10 22:23:36 -0300

    feat: add findAll method with Pageable to ProductService

--- src/main/java/com/josemneto/cartflow/service/ProductService.java
+++ src/main/java/com/josemneto/cartflow/service/ProductService.java
@@ -3,9 +3,12 @@ package com.josemneto.cartflow.service;
 import com.josemneto.cartflow.dto.ProductDTO;
 import com.josemneto.cartflow.entities.Product;
 import com.josemneto.cartflow.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -23,5 +26,11 @@ public class ProductService {
         Product product = result.get();
         ProductDTO dto = new ProductDTO(product);
         return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ProductDTO> findAll(Pageable pageable) {
+        Page<Product> result = repository.findAll(pageable);
+        return result.map(x -> new ProductDTO(x));
     }
 }
